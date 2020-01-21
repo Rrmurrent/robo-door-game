fedaae299999dd5d26d0323f31ec7718f5fdde69
Smarty Pants <amcurrent@hotmail.com>
2020-01-21 08:46:20 -0600

    added Math.floor(Math.random) to js file, to make the robot change doors every time.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,48 @@ let friendlyBotPath = 'https://pbs.twimg.com/profile_images/378800000481581818/2
 
 let evilBot = "https://pbs.twimg.com/profile_images/378800000127515414/ad6bd483b5d47dd9454cc013c19e9dd6.png";
 
+let numClosedDoors = 3;
 
+let openDoor1;
+
+let openDoor2;
+
+let openDoor3;
+
+const randomBotDoorGenerator = () => {
+  const botDoor = Math.floor(Math.random() * numClosedDoors);
+  if (botDoor === 0) {
+    openDoor1 = evilBot;
+    openDoor2 = niceBotPath;
+    openDoor3 = friendlyBotPath;
+  } else if (botDoor === 1) {
+    openDoor2 = evilBot;
+    openDoor1 = niceBotPath;
+    openDoor3 = friendlyBotPath;
+  } else {
+    openDoor3 = evilBot;
+    openDoor1 = niceBotPath;
+    openDoor2 = friendlyBotPath;
+  }
+}
 
 
 doorImage1.onclick = () => {
 
-  let botDoorPath = evilBot
-  doorImage1.src = botDoorPath;
+  // let botDoorPath = evilBot
+  doorImage1.src = openDoor1;
 }
 
 doorImage2.onclick = () => {
 
-  let botDoorPath = friendlyBotPath;
-  doorImage2.src = botDoorPath;
+  // let botDoorPath = friendlyBotPath;
+  doorImage2.src = openDoor2;
 }
 
 doorImage3.onclick = () => {
 
-  let botDoorPath = niceBotPath;
-  doorImage3.src = botDoorPath;
+  // let botDoorPath = niceBotPath;
+  doorImage3.src = openDoor3;
 }
+
+randomBotDoorGenerator();
